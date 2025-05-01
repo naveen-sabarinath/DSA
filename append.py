cfd86d8e8142f6516d8743350b565779387629d5
naveen-sabarinath <naveensabaribabu@gmail.com>
2025-05-01 17:01:14 +0530

    added the pop function

--- append.py
+++ append.py
@@ -1,12 +1,12 @@
 from baseLL import Node 
 class LinkedList:
-     def __init__(self,value):
-          self.head = Node(value)
+     def __init__(self,data):
+          self.head = Node(data)
           self.tail = self.head
           self.size =1
      def print_linkedlist(self):
            temp = self.head
-           while self.size is not None:
+           while self.size !=0:
                  print(temp.data)
                  temp = temp.next
 
@@ -21,10 +21,27 @@ class LinkedList:
                     self.tail.next = node
                     self.tail = node
                     self.size += 1
+     def pop(self):
+           if self.size ==0:
+                 return None
+           if self.size ==1:
+                 temp = self.head
+                 self.head =None
+                 self.tail = None
+                 self.size =0
+                 return temp
+           else:
+                 temp = self.tail
+                 current = self.head
+                 while current.next != temp:
+                    current = current.next
+                    self.tail = current
+                    self.tail.next = None
+                    self.size -= 1
+
+                 
 
 my_list = LinkedList(5)
 print(my_list.head.data) # 5
 my_list.append(10)
-print(my_list.tail.data) # 10
-print(my_list.size) # 2 
-my_list.print_linkedlist() # 5 10
+my_list.pop()
